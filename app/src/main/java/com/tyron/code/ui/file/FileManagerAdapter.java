079055f51194351594db53c23d634d0a97947314
evarea-app <86873019+evarea-app@users.noreply.github.com>
2022-02-15 17:03:10 +0300

    Update FileManagerAdapter.java

--- app/src/main/java/com/tyron/code/ui/file/FileManagerAdapter.java
+++ app/src/main/java/com/tyron/code/ui/file/FileManagerAdapter.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.tyron.code.R;
